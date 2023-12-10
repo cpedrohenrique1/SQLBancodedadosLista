4e3b2599359705bc7f44d6faf6df71450bfff3f5
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2023-12-10 16:34:08 -0300

    Adicao de comentarios, termino de exercicios lista 2 e criacao de lista3

--- lista1.sql
+++ lista1.sql
@@ -1,10 +1,10 @@
--- 1
+-- 1 Criar um banco de dados Projetos
 CREATE DATABASE Projetos;
 
 \c Projetos;
 
--- 2
--- a
+-- 2 Crie o seguinte esquema do banco de dados Projetos
+-- a Empregado (nome, cpf, data-nasc, sexo, salario, cpf_supervisor, dept)
 CREATE TABLE Empregado (
 	nome varchar(255) NOT NULL,
     cpf int PRIMARY KEY NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE Empregado (
     dept int
 );
 
--- b
+-- b Departamento (nome, numero, cpf_gerente, data_inicio_gerencia)
 CREATE TABLE Departamento (
 	nome varchar(255) NOT NULL,
     numero int PRIMARY KEY NOT NULL,
@@ -23,14 +23,14 @@ CREATE TABLE Departamento (
     data_inicio_gerencia date NOT NULL
 );
 
--- c
+-- c Projeto (nome, numero, dept)
 CREATE TABLE Projeto (
     nome varchar(255) NOT NULL,
     numero int PRIMARY KEY NOT NULL,
     dept int NOT NULL
 );
 
--- d
+-- d Trabalha_Para (empregado, projeto, horas)
 CREATE TABLE Trabalha_Para (
     empregado int NOT NULL,
     projeto int NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE Trabalha_Para (
     FOREIGN KEY (projeto) REFERENCES Projeto(numero)
 );
 
--- e
+-- e Gerenciou (cpf, num-dept)
 CREATE TABLE Gerenciou (
     cpf int,
     num_dep int,
@@ -49,7 +49,7 @@ CREATE TABLE Gerenciou (
     FOREIGN KEY (num_dep) REFERENCES Departamento(numero)
 );
 
--- 3
+-- 3 Inserir os seguintes registros em empregado:
 INSERT INTO Empregado (nome, cpf, data_nasc, sexo, salario, cpf_supervisor, dept)
 VALUES
 ('Arnaldo Silva', 123456, '1928-02-21', 'm', 4000, 333333, 5),
@@ -59,14 +59,14 @@ VALUES
 ('Eduardo Dias', 555555, '1955-05-06', 'm', 2500, NULL, 2),
 ('Fernanda Cabral', 444444, '1966-07-17', 'f', 3500, NULL, 1);
 
--- 4
+-- 4 Inserir os seguintes registros em Departamento:
 INSERT INTO Departamento (nome, numero, cpf_gerente, data_inicio_gerencia)
 VALUES
 ('Pesquisa', 5, 333333, '1985-06-22'),
 ('Administracao', 2, 555555, '1989-08-04'),
 ('Ensino', 1, 444444, '1992-09-29');
 
--- 5
+-- 5 Inserir os seguintes registros em Projeto:
 INSERT INTO Projeto (nome, numero, dept)
 VALUES
 ('P1', 1, 5),
@@ -76,7 +76,7 @@ VALUES
 ('P3', 5, 5),
 ('P2', 6, 2);
 
--- 6
+-- 6 Inserir os seguintes registros em Gerenciou
 INSERT INTO Gerenciou (cpf, num_dep)
 VALUES
 (444444, 1),
@@ -85,7 +85,7 @@ VALUES
 (555555, 2),
 (444444, 5);
 
--- 7
+-- 7 Inserir os seguintes registros em Trabalha_Para
 INSERT INTO Trabalha_Para (empregado, projeto, horas)
 VALUES
 (123456, 1, 12),
--- lista2.sql
+++ lista2.sql
@@ -1,43 +1,117 @@
--- 1
+-- 1 Mostre os dados dos funcionários do depto 5 que são mulheres e não ganham mais de 2500
 SELECT * FROM Empregado
 WHERE dept = 5
 AND sexo = 'f'
 AND salario <= 2500;
 
--- 2
-SELECT * FROM Empregado;
+-- 2 Mostre os nomes e salários de todos os funcionários
+SELECT nome, salario FROM Empregado;
 
--- 3
-SELECT nome, salario FROM Empregado
+-- 3 Liste o nome e o salário dos funcionários do departamento 2
+SELECT nome, salario
+FROM Empregado
 WHERE dept = 2;
 
--- 4
-SELECT DISTINCT em.nome
+-- 4 Mostre o nome do gerente do departamento 1
+SELECT em.nome
 FROM Empregado AS em
-LEFT JOIN  Departamento AS de ON de.numero = dept
+LEFT JOIN Departamento AS de ON de.numero = em.dept
 WHERE de.numero = 1
-AND cpf_gerente = em.cpf;
+AND em.cpf = de.cpf_gerente;
 
--- 5
+-- 5 Liste os CPFs dos funcionários que são gerentes
 SELECT cpf_gerente
 FROM Departamento;
 
--- 6
+-- 6 Mostre os CPFs dos que não são gerentes
 SELECT em.cpf FROM Empregado AS em
 LEFT JOIN Departamento as de ON em.cpf = de.cpf_gerente
 WHERE de.cpf_gerente IS NULL;
 
--- 7
+-- 7 Liste os CPFs dos funcionários que são gerentes ou que ganham acima de 3000
 SELECT de.cpf_gerente FROM Departamento AS de
 WHERE de.cpf_gerente IS NOT NULL
 UNION
 SELECT cpf FROM Empregado
 WHERE salario > 3000;
 
--- 8
-SELECT em.nome, em.salario FROM Empregado AS em
+-- 8 Mostre os nomes e os salários dos empregados do departamento de ensino
+SELECT em.nome, em.salario 
+FROM Empregado AS em
 LEFT JOIN Departamento AS de ON em.dept = de.numero
 WHERE de.nome LIKE '%Ensino%';
 
--- 9
-SELECT 
+-- 9 Mostre os nomes dos empregados que trabalham no projeto número 1
+SELECT em.nome FROM Empregado AS em
+LEFT JOIN Trabalha_Para AS tp ON em.cpf = tp.empregado
+WHERE tp.projeto = 1;
+
+-- 10 Liste os CPFs dos empregados que trabalham em todos os projetos do departamento 5
+SELECT em.cpf
+FROM Empregado AS em
+WHERE em.dept = 5
+AND NOT EXISTS (
+    SELECT p.numero
+    FROM Projeto AS p
+    WHERE NOT EXISTS (
+        SELECT tp.projeto
+        FROM Trabalha_Para AS tp
+        WHERE tp.empregado = em.cpf
+        AND tp.projeto = p.numero
+    )
+);
+
+-- 11 Mostre os nomes dos empregados supervisionados por Carlos Pedrosa
+SELECT em.nome
+FROM Empregado AS em
+WHERE em.cpf_supervisor = 333333;
+
+-- 12 Liste os nomes dos empregados que não trabalham em qualquer projeto
+SELECT em.nome
+FROM Empregado AS em
+WHERE NOT EXISTS (
+    SELECT tp.empregado
+    FROM Trabalha_Para AS tp
+    WHERE tp.empregado = em.cpf
+);
+
+-- 13 Mostre CPFs dos que trabalham em pelo menos um projeto
+SELECT DISTINCT em.nome
+FROM Empregado AS em
+LEFT JOIN Trabalha_Para AS tp
+ON em.cpf = tp.empregado;
+
+-- 14 Mostre os CPFs dos empregados que gerenciam algum departamento ou que não participam de qualquer projeto
+SELECT de.cpf_gerente
+FROM Departamento AS de
+WHERE de.cpf_gerente IS NOT NULL
+UNION
+SELECT em.cpf
+FROM Empregado AS em
+WHERE em.cpf NOT IN (
+    SELECT DISTINCT tp.empregado
+    FROM Trabalha_Para AS tp
+);
+
+-- 15 Mostre o valor total dos salários em cada departamento
+SELECT dept, SUM(salario) AS soma_salarios
+FROM Empregado
+GROUP BY dept;
+
+-- 16 Mostre a média dos salários pagos por departamento que possua mais de 5 empregados
+SELECT dept, AVG(salario) AS media_salarios
+FROM Empregado
+GROUP BY dept
+HAVING COUNT(*) > 5;
+
+-- 17 Liste o maior salário de mulher de cada departamento com mais de 5 empregados do sexo feminino
+SELECT dept, MAX(salario) AS maior_salario
+FROM Empregado
+WHERE sexo = 'f' AND dept IN (
+    SELECT dept
+    FROM Empregado
+    WHERE sexo = 'f'
+    GROUP BY dept
+    HAVING COUNT(*) > 5
+)
+GROUP BY dept;
